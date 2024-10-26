b04c1e527da16d4a62746f98087a0fd1b449b587
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2024-10-26 12:24:38 -0700

    began implementing Truck class

--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -6,8 +6,10 @@ import Wheel from './Wheel.js';
 import AbleToTow from '../interfaces/AbleToTow.js';
 
 // TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
-class Truck extends Vehicle {
+class Truck extends Vehicle implements AbleToTow {
   // TODO: Declare properties of the Truck class
+  // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
+  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
   vin: string;
   color: string;
   make: string;
@@ -17,14 +19,22 @@ class Truck extends Vehicle {
   topSpeed: number;
   wheels: Wheel[];
   towingCapacity: string;
-  // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
-  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
 
   // TODO: Create a constructor that accepts the properties of the Truck class
     // TODO: The constructor should call the constructor of the parent class, Vehicle
     // TODO: The constructor should initialize the properties of the Truck class
     // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
-
+constructor(
+  vin: string,
+  color: string,
+  make: string,
+  model: string,
+  year: number,
+  weight: number,
+  topSpeed: number,
+  wheels: Wheel[],
+  towingCapacity: string
+)
   // TODO: Implement the tow method from the AbleToTow interface
   tow(vehicle: Truck | Motorbike | Car): void {
     
@@ -36,8 +46,25 @@ class Truck extends Vehicle {
 
     // TODO: Override the printDetails method from the Vehicle class
     // TODO: The method should call the printDetails method of the parent class
-    // TODO: The method should log the details of the Truck
-    // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
+    override printDetails(): void {
+      // TODO: The method should log the details of the Truck
+      super.printDetails();
+      // TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
+      console.log(`VIN: ${this.vin}`);
+      console.log(`Color: ${this.color}`);
+      console.log(`Make: ${this.make}`);
+      console.log(`Model: ${this.model}`);
+      console.log(`Year: ${this.year}`);
+      console.log(`Weight: ${this.weight} lbs`);
+      console.log(`Top Speed: ${this.topSpeed} mph`);
+      console.log(`Towing Capacity: ${this.towingCapacity}`);
+      console.log(
+        `Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`
+      );
+      console.log(
+        `Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`
+      );
+    }
 }
 
 // Export the Truck class as the default export
